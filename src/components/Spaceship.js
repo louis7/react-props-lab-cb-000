5b89029312f9e80afc339cea7e0944458131f142
louis baptiste <louisvis@ymail.com>
2020-06-20 19:11:41 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,4 +1,20 @@
 // Code The Spaceship Component Here
-export default class Spaceship extends React.Component { ... };
+
+
+
+
+export default class Spaceship extends React.Component {
+render(){
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+
+
+
+   ... };
 // OR declare your component first, and then:
 export default Spaceship;
